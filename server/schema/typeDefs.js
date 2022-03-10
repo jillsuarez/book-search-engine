1e4e6173a5d8ec5ee7732035ece5b004344aaaf8
jillsuarez <jillsuarez@live.com>
2022-03-10 11:31:02 -0500

    wireframe code for schema

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User{
+
+    }
+
+    type Book {
+
+    }
+
+    type Auth {
+
+    }
+
+    type Query {
+
+    }
+
+    type Mutation {
+        
+    }
+`
+
+module.exports = typeDefs;

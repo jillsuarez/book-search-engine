9258b7c37a3efb668a54ec6fc49d39161e8bc98c
jillsuarez <jillsuarez@live.com>
2022-03-10 13:36:20 -0500

    added code to resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -4,11 +4,58 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-
+        me: async(parents, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({ _id: context.user._id}).populate("savedbooks");
+                return userData;
+            }
+            throw new AuthenticationError("Not logged in");
+        },
     },
 
     Mutation: {
-        
+        addUser: async(parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async(parent, { email, password }) => {
+            const user = await User.findOne({email});
+
+            if(!user) {
+                throw new AuthenticationError("Incorrect Credientials");
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$push: {savedBooks: args}},
+                    {new: true, runValidator: true}
+                );
+                return updateUser;
+            }
+            throw new AuthenticationError('You need to be logged in.');
+        },
+        removeBook: async (parent, { bookId }, context ) => {
+            if(context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$pull: { savedBooks: { bookId: bookId }}},
+                    {new: true}
+                );
+                return updateUser;
+            }
+            throw new AuthenticationError('You need to be logged in.');
+        }
     }
 
 }
